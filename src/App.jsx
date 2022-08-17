31f0b9f6120846966cd65cbcad7b915c79ed7d6d
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-08-17 23:47:13 +0300

    installed some deps

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import './App.css'
+import Typography from '@mui/material/Typography';
 
 function App() {
 
   return (
     <div className="App">
+      <Typography variant="h1" gutterBottom>
       Project works
+      </Typography>
     </div>
   )
 }
