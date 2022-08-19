84d470950295e7903826ff9dc1ca285f8f9a6aac
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-08-19 16:42:26 +0300

    added consts for routes

---
+++ src/utils/consts.js
@@ -0,0 +1,8 @@
+export const ADMIN_ROUTE = "/admin";
+export const LOGIN_ROUTE = "/login";
+export const REGISTRATION_ROUTE = "/registration";
+export const BOOK_ROUTE = "/book";
+export const MAIN_ROUTE = "/";
+export const ORDER_ROUTE = "/order";
+export const PROFILE_ROUTE = "/profile";
+export const WISHLIST_ROUTE = "/wishlist";
