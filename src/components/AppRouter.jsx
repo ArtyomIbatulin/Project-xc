65846e98ab950b2febd5b8ec88b7942e82e40b28
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-09-19 23:39:29 +0300

    added navigate route

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { authRoutes, publicRoutes } from "../routes";
 
 export const AppRouter = () => {
@@ -9,12 +9,12 @@ export const AppRouter = () => {
     <Routes>
       {isAuth &&
         authRoutes.map(({ path, Component }) => (
-          <Route key={path} path={path} element={<Component />} exact />
+          <Route key={path} path={path} element={<Component />} />
         ))}
       {publicRoutes.map(({ path, Component }) => (
-        <Route key={path} path={path} element={<Component />} exact />
+        <Route key={path} path={path} element={<Component />} />
       ))}
-      // redirect в новом rrd
+      {<Route path="*" element={<Navigate to="/" replace />} />}
     </Routes>
   );
 };
