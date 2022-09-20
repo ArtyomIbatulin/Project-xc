b750ec6497e82ff0e6e4e3075edfadcccaf94de6
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-09-20 18:26:21 +0300

    fixed user slice(exist err)

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,7 +1,16 @@
-import React from 'react'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { isAuth } from "../store/features/user/userSlice";
 
 export const MainPage = () => {
+  const isAuth = useSelector((state) => state.user.isAuth);
+  const dispatch = useDispatch();
+
   return (
-    <div>MainPage</div>
-  )
-}
+    <>
+      <div>MainPage</div>
+      <h2>State - {isAuth}</h2>
+      <button onClick={() => dispatch(isAuth())}>Change state</button>
+    </>
+  );
+};
