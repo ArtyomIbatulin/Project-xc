8607865ef96ed84afca90b89b4c63927a6ad4326
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-11-01 19:58:01 +0300

    fixed styles auth page

--- src/pages/Auth.jsx
+++ src/pages/Auth.jsx
@@ -1,13 +1,17 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Container from "@mui/material/Container";
 import TextField from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { NavLink } from "react-router-dom";
+import { REGISTRATION_ROUTE } from "../utils/consts";
 
 export const Auth = () => {
   const [values, setValues] = React.useState({
@@ -31,40 +35,57 @@ export const Auth = () => {
   };
 
   return (
-    <Container maxWidth="lg">
+    <Container
+      maxWidth="lg"
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        mt: 10,
+      }}
+    >
       <Box
         component="form"
         sx={{
           "& > :not(style)": { m: 1, width: "25ch" },
+          border: "1px solid gray",
+          borderRadius: 5,
+          padding: 5,
         }}
       >
-        <TextField
-          id="login"
-          placeholder="Введите ваш login"
-          variant="outlined"
-        />
+        <Typography variant="h6">Авторизация</Typography>
 
-        {/* <InputLabel htmlFor="input-password">Введите пароль</InputLabel> */}
+        <Stack spacing={2} direction="column">
+          <TextField
+            id="login"
+            placeholder="Введите ваш login"
+            variant="outlined"
+          />
 
-        <OutlinedInput
-          id="input-password"
-          placeholder="Введите ваш пароль"
-          type={values.showPassword ? "text" : "password"}
-          value={values.password}
-          onChange={handleChange("password")}
-          endAdornment={
-            <InputAdornment position="end">
-              <IconButton
-                aria-label="toggle password visibility"
-                onClick={handleClickShowPassword}
-                onMouseDown={handleMouseDownPassword}
-                edge="end"
-              >
-                {values.showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          }
-        />
+          <OutlinedInput
+            id="input-password"
+            placeholder="Введите ваш пароль"
+            type={values.showPassword ? "text" : "password"}
+            value={values.password}
+            onChange={handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </Stack>
+
+        <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся</NavLink>
+
+        <Button variant="outlined">Войти</Button>
       </Box>
     </Container>
   );
