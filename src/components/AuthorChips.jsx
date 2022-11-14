c47591c68bbff521b58ddbcf7ffcef020a06a2c4
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-11-14 20:59:27 +0300

    added quick react slick

--- src/components/AuthorChips.jsx
+++ src/components/AuthorChips.jsx
@@ -3,6 +3,11 @@ import { useSelector } from "react-redux";
 import Chip from "@mui/material/Chip";
 import Stack from "@mui/material/Stack";
 
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+import Slider from "react-slick";
+
 export const AuthorChips = () => {
   const author = useSelector((state) => state.author);
 
@@ -10,12 +15,46 @@ export const AuthorChips = () => {
     console.log(e.target.innerText);
   };
 
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
   return (
-    <Stack direction="row" spacing={1} mt={1}>
-      {author.map((author) => (
-        <Chip key={author.id} label={author.name} onClick={handleClick} />
-      ))}
-    </Stack>
+    <>
+      <Stack direction="row" spacing={1} mt={1}>
+        {author.map((author) => (
+          <Chip key={author.id} label={author.name} onClick={handleClick} />
+        ))}
+      </Stack>
+
+      <div>
+        <h2> Single Item</h2>
+        <Slider {...settings}>
+          <div>
+            <h3>1</h3>
+          </div>
+          <div>
+            <h3>2</h3>
+          </div>
+          <div>
+            <h3>3</h3>
+          </div>
+          <div>
+            <h3>4</h3>
+          </div>
+          <div>
+            <h3>5</h3>
+          </div>
+          <div>
+            <h3>6</h3>
+          </div>
+        </Slider>
+      </div>
+    </>
   );
 };
 
