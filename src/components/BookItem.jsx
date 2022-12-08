e7ec85ee50e19a160453dbb30f1e0d34d4273449
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-12-08 20:22:40 +0300

    created books card

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 export const BookItem = ({ item }) => {
   return (
     <>
       <Grid item md={3}>
-        item
+        <Card sx={{ maxWidth: 345 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={item.img}
+            alt="book img"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.name}
+            </Typography>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.price}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Like it</Button>
+          </CardActions>
+        </Card>
       </Grid>
     </>
   );
