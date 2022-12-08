42541816743541169038ab3f6f5d372e4ef1c010
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-12-08 21:07:02 +0300

    styled books img

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -22,7 +22,7 @@ export const BookItem = ({ item }) => {
             <Typography gutterBottom variant="h5" component="div">
               {item.name}
             </Typography>
-            <Typography gutterBottom variant="h5" component="div">
+            <Typography gutterBottom variant="h6" component="div">
               {item.price}
             </Typography>
             <Typography variant="body2" color="text.secondary">
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -14,3 +14,5 @@ export const BookList = () => {
     </Stack>
   );
 };
+
+// Grid container ?
