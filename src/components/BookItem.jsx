875d4a292470dfb58d395a15d2fac6b1625fa6bd
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-12-09 17:07:28 +0300

    fixing grid bookItem

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -10,29 +10,31 @@ import Typography from "@mui/material/Typography";
 export const BookItem = ({ item }) => {
   return (
     <>
-      <Grid item md={3}>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardMedia
-            component="img"
-            height="140"
-            image={item.img}
-            alt="book img"
-          />
-          <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
-              {item.name}
-            </Typography>
-            <Typography gutterBottom variant="h6" component="div">
-              {item.price}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              {item.description}
-            </Typography>
-          </CardContent>
-          <CardActions>
-            <Button size="small">Like it</Button>
-          </CardActions>
-        </Card>
+      <Grid container>
+        <Grid item lg={4} md={3}>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="140"
+              image={item.img}
+              alt="book img"
+            />
+            <CardContent>
+              <Typography gutterBottom variant="h5" component="div">
+                {item.name}
+              </Typography>
+              <Typography gutterBottom variant="h6" component="div">
+                {item.price}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                {item.description}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              <Button size="small">Like it</Button>
+            </CardActions>
+          </Card>
+        </Grid>
       </Grid>
     </>
   );
