1e26c8f20e0d0f277b0fb28047cfa0f3af4e5441
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-12-13 00:50:23 +0300

    fixed grid bookList & bookItem

--- src/components/BookItem.jsx
+++ src/components/BookItem.jsx
@@ -10,31 +10,29 @@ import Typography from "@mui/material/Typography";
 export const BookItem = ({ item }) => {
   return (
     <>
-      <Grid container>
-        <Grid item lg={4} md={3}>
-          <Card sx={{ maxWidth: 345 }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image={item.img}
-              alt="book img"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                {item.name}
-              </Typography>
-              <Typography gutterBottom variant="h6" component="div">
-                {item.price}
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                {item.description}
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Like it</Button>
-            </CardActions>
-          </Card>
-        </Grid>
+      <Grid item>
+        <Card sx={{ maxWidth: 350 }}>
+          <CardMedia
+            component="img"
+            height="140"
+            image={item.img}
+            alt="book img"
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              {item.name}
+            </Typography>
+            <Typography gutterBottom variant="h6" component="div">
+              {item.price}
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              {item.description}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            <Button size="small">Like it</Button>
+          </CardActions>
+        </Card>
       </Grid>
     </>
   );
--- src/components/BookList.jsx
+++ src/components/BookList.jsx
@@ -2,17 +2,18 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { BookItem } from "./BookItem";
 import Stack from "@mui/material/Stack";
+import Grid from "@mui/material/Grid";
 
 export const BookList = () => {
   const book = useSelector((state) => state.book);
 
   return (
     <Stack direction="row" spacing={1} mt={1}>
-      {book.map((item) => (
-        <BookItem key={item.id} item={item} />
-      ))}
+      <Grid container spacing={2}>
+        {book.map((item) => (
+          <BookItem key={item.id} item={item} />
+        ))}
+      </Grid>
     </Stack>
   );
 };
-
-// Grid container ?
