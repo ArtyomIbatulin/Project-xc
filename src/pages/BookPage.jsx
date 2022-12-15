9fa35dd8739f6a347bd13f489cd1d30507a44aa4
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-12-16 00:17:13 +0300

    removed mui-image

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -3,17 +3,19 @@ import Box from "@mui/material/Box";
 
 export const BookPage = () => {
   return (
-    <Box
-      component="img"
-      sx={{
-        mt: 1,
-        height: 233,
-        width: 350,
-        maxHeight: { xs: 233, md: 167 },
-        maxWidth: { xs: 350, md: 250 },
-      }}
-      alt="image"
-      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbQ7qVM6wbOh3pQJgnqqJTMXX7QVXqNcC5AA&usqp=CAU"
-    />
+    <>
+      <Box
+        component="img"
+        sx={{
+          mt: 1,
+          height: 233,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+        }}
+        alt="image"
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbQ7qVM6wbOh3pQJgnqqJTMXX7QVXqNcC5AA&usqp=CAU"
+      />
+    </>
   );
 };
