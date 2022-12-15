58038df7588282d467769ddbc23b56fe39031fe7
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-12-15 23:19:37 +0300

    installed mui-image

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from "react";
+import Box from "@mui/material/Box";
 
 export const BookPage = () => {
   return (
-    <div>BookPage</div>
-  )
-}
+    <Box
+      component="img"
+      sx={{
+        mt: 1,
+        height: 233,
+        width: 350,
+        maxHeight: { xs: 233, md: 167 },
+        maxWidth: { xs: 350, md: 250 },
+      }}
+      alt="image"
+      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbQ7qVM6wbOh3pQJgnqqJTMXX7QVXqNcC5AA&usqp=CAU"
+    />
+  );
+};
