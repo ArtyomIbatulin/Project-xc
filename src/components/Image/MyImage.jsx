953c901c4a7ecac6ada5bdf563b1aa4f38a8097e
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-12-20 19:44:19 +0300

    created comp myImage

---
+++ src/components/Image/MyImage.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import Box from "@mui/material/Box";
+
+export const MyImage = (props) => {
+  return <Box component="img" {...props} />;
+};
--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import Box from "@mui/material/Box";
+import { MyImage } from "../components/Image/MyImage";
 
 export const BookPage = () => {
   return (
     <>
-      <Box
-        component="img"
+      <MyImage
         sx={{
           mt: 1,
-          height: 233,
-          width: 350,
-          maxHeight: { xs: 233, md: 167 },
-          maxWidth: { xs: 350, md: 250 },
+          // height: 200,
+          // width: 400,
+          // maxHeight: { xs: 233, md: 167 },
+          // maxWidth: { xs: 350, md: 250 },
         }}
         alt="image"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbQ7qVM6wbOh3pQJgnqqJTMXX7QVXqNcC5AA&usqp=CAU"
