4f60140a2bb1c33b4f588f886558414eb4a0ad21
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2022-12-26 19:08:59 +0300

    created book inf

--- src/pages/BookPage.jsx
+++ src/pages/BookPage.jsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { MyImage } from "../components/Image/MyImage";
 
+const book = {
+  id: 1,
+  author: "Толкиен",
+  name: "Rings",
+  price: 1000,
+  description: "Описание книги",
+  img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbQ7qVM6wbOh3pQJgnqqJTMXX7QVXqNcC5AA&usqp=CAU",
+  category: "Фантастика",
+  commentText: "Текст комментария",
+  date: "Дата в формате даты",
+  rating: "5",
+};
+
 export const BookPage = () => {
   return (
     <>
       <MyImage
         sx={{
           mt: 1,
-          // height: 200,
-          // width: 400,
-          // maxHeight: { xs: 233, md: 167 },
-          // maxWidth: { xs: 350, md: 250 },
         }}
         alt="image"
         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRbQ7qVM6wbOh3pQJgnqqJTMXX7QVXqNcC5AA&usqp=CAU"
